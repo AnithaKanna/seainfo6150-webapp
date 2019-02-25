9076bad6348a57b97efec39aff993c026a5b5956
Anitha Kanna <anithakanna@Anithas-MacBook-Pro.local>
2019-02-24 16:14:18 -0800

    [Adding_article7]

---
+++ assignment3/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import ArticleGrid from './ArticleGrid';
+//import articles from './articles.json';
+
+
+class App extends Component {
+
+    render() {
+      return (
+
+        <div>
+
+        <ArticleGrid />
+
+      </div>
+    );
+  }
+}
+
+export default App;
