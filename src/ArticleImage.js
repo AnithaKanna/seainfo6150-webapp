b86073c0b4309f02c67d7d449994678765203217
Anitha Kanna <anithakanna@Anithas-MBP.hitronhub.home>
2019-03-04 21:14:10 -0800

    added assignment8

--- src/ArticleImage.js
+++ src/ArticleImage.js
@@ -2,13 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './ArticleImage.css';
 
-class ArticleImage extends React.Component{
+class ArticleImage extends React.Component {
     render() {
       return(
-            <img className='img' src= {this.props.url} alt="check"></img>
+            <img className='img' src={this.props.url} alt="check" />
           );
+	}
 }
-}
+
 ArticleImage.propTypes = {
   url: PropTypes.string.isRequired
 };
